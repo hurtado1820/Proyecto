7787be9b707e7ae70ca18a38d94cfeaef877cd90
Djkde <sergio.estrella@utp.edu.co>
2020-06-17 00:03:41 -0500

    Añadida música (No está en el repo por ser muy pesada)

--- mainJuego.py
+++ mainJuego.py
@@ -14,23 +14,38 @@ if __name__ == '__main__':
     pygame.init()
     ventana = pygame.display.set_mode([ANCHO,ALTO])
     reloj = pygame.time.Clock()
+    win = pygame.mixer.Sound("WinMenu.wav")
+    firstLevel = pygame.mixer.Sound("FirstLevel.wav")
+    secondLevel = pygame.mixer.Sound("SecondLevel.wav")
+    death = pygame.mixer.Sound("LoseMenu.wav")
+    start = pygame.mixer.Sound("MainMenu.wav")
+    pygame.mixer.music.load
     #Pantalla inicio
+    start.play()
     Inicio(ventana)
     #Tutorial de juego
     Tutorial(ventana)
     #Historia nivel 1
     H1(ventana)
+    start.stop()
     #Llamado a primer nivel
+    firstLevel.play()
     primer = Nivel1(ventana)
     # 1 perdio 0 gano
+    firstLevel.stop()
     if primer == 1:
+        death.play()
         Perdida(ventana)
     elif primer == 0:
         #Historia nivel 2
         H2(ventana)
         #Llamado a segundo nivel
+        secondLevel.play()
         segundo = Nivel2(ventana)
+        secondLevel.stop()
         if segundo == 0:
+            win.play()
             Victoria(ventana)
         else:
+            death.play()
             Perdida(ventana)
